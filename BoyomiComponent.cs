0d4cf31e2c11bf03ed962d1f4cad86cd282159a1
torisoup <tori.birdstrike@gmail.com>
2014-07-17 01:18:29 +0900

    First commit.

---
+++ BoyomiComponent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Unityで使う方
+/// </summary>
+public class BoyomiComponent : MonoBehaviour
+{
+    BoyomiClient boyomi;
+    public string hostIp = "127.0.0.1";
+    public int hostPort = 50001;
+
+    //何秒おきに棒読みちゃん問い合わせるか
+    public float checkSecond = 0.2f;
+
+    /// <summary>
+    /// 発声状態
+    /// </summary>
+    public bool isSpeaching
+    {
+        get
+        {
+            if (boyomi == null) { return false; }
+            return boyomi.isSpeaching;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.boyomi = new BoyomiClient(hostIp, hostPort);
+        InvokeRepeating("checkSpeacking", checkSecond, checkSecond);
+	}
+
+    void checkSpeacking()
+    {
+        this.boyomi.CheckSpeaking();
+    }
+}
---
+++ Boyomichan/BoyomiClient.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+/// <summary>
+/// Unity非依存なSocket通信部分
+/// </summary>
+class BoyomiClient
+{
+    private TcpClient tcpClient;
+    private String hostIp;
+    private int hostPort;
+    byte[] readbuf;
+    bool _isSpeaching;
+
+    /// <summary>
+    /// 発声中であるか
+    /// </summary>
+    public bool isSpeaching
+    {
+        get
+        {
+            return this._isSpeaching;
+        }
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="hostIp">ホストIP</param>
+    /// <param name="hostPort">ホストポート</param>
+    public BoyomiClient(string hostIp,int hostPort)
+    {
+        this.hostIp = hostIp;
+        this.hostPort = hostPort;
+        readbuf = new byte[1024];
+    }
+
+    /// <summary>
+    /// 棒読みちゃんに接続を試みる
+    /// </summary>
+    public void Connect()
+    {
+        try
+        {
+            tcpClient = new TcpClient(hostIp, hostPort);
+            //コールバック指定
+            tcpClient.GetStream().BeginRead(readbuf, 0, readbuf.Length, CallBackBeginReceive, null);
+        }
+        catch (Exception e)
+        {
+            //nice catch!
+        }
+    }
+
+    /// <summary>
+    /// 受信CallBack
+    /// 発声中であるかどうかしか確認しない
+    /// </summary>
+    /// <param name="ar">IAsyncResult</param>
+    private void CallBackBeginReceive(IAsyncResult ar)
+    {
+        var bytes = tcpClient.GetStream().EndRead(ar);
+        //結果が0でないなら発声中
+        _isSpeaching = readbuf[0] > 0;
+    }
+
+    /// <summary>
+    /// 棒読みちゃん発声状態を問い合わせる
+    /// 結果は最終的に_isSpeachingに入る
+    /// </summary>
+    public void CheckSpeaking()
+    {
+        //一回のRequestごとに接続が切れるので毎回つなぐ
+        Connect();
+        try
+        {
+            using (NetworkStream ns = tcpClient.GetStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ns))
+                {
+                    //0x0120はGetNowPlaying（音声再生状態の取得）
+                    Int16 iCommand = 0x0120;
+                    bw.Write(iCommand);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            //nice catch!
+        }
+    }
+}
+
